f33c2de786ebd9d6c74fca19e3a847c08c608d29
Sindujan <sindujan@paraqum.com>
2023-09-13 11:33:12 +0530

    graphql config and connection

--- lib/action.ts
+++ lib/action.ts
@@ -1,14 +1,40 @@
+import { createUserMutation, getUserQuery } from "@/graphql";
 import { GraphQLClient } from "graphql-request";
 
 
-const isProduction = process.env.NODE_ENV ==='production'
+const isProduction = process.env.NODE_ENV === 'production'
 
-const makeGraphQLRequest = async (quary: string,
+const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : "http://127.0.0.1:4000/graphql"
+
+const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '' : "ketmain";
+
+const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL : "http://localhost:3000";
+
+const client = new GraphQLClient(apiUrl)
+
+const makeGraphQLRequest = async (query: string,
     variables = {}) => {
 
     try {
-
+        return await client.request(query, variables)
     } catch (err) {
         throw err
     }
+}
+
+
+export const getUser = (email: string) => {
+    return makeGraphQLRequest(getUserQuery, { email })
+}
+
+export const createUser=(name:string,email:string,avatarUrl:string)=>{
+    const variables ={
+        input:{
+            name,email,avatarUrl
+        }
+    }
+
+    return makeGraphQLRequest(createUserMutation,{
+       variables
+    })
 }
--- lib/session.ts
+++ lib/session.ts
@@ -1,17 +1,18 @@
 import { getServerSession } from "next-auth";
-import { NextAuthOptions,User } from "next-auth";
+import { NextAuthOptions, User } from "next-auth";
 import { AdapterUser } from "next-auth/adapters";
 import GoogleProvider from 'next-auth/providers/google'
 import jsonwebtoken from 'jsonwebtoken'
 import { JWT } from "next-auth/jwt";
-import { SessionInterface } from "@/common.types";
+import { SessionInterface, UserProfile } from "@/common.types";
+import { createUser, getUser } from "./action";
 
 
-export const authOption : NextAuthOptions={
-    providers : [
+export const authOption: NextAuthOptions = {
+    providers: [
         GoogleProvider({
-            clientId : process.env.GOOGLE_CLIENT_ID!,
-            clientSecret:process.env.GOOGLE_CLIENT_SECRET!
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
         })
     ],
     // jwt :{
@@ -19,33 +20,54 @@ export const authOption : NextAuthOptions={
 
     //     },
     //     decode: async({secret,token})=>{
-            
+
     //     }
     // },
-    theme:{
-        colorScheme : "light",
+    theme: {
+        colorScheme: "light",
         logo: '/logo.png'
     },
-    callbacks : {
-        async session({session}) {
-            return session;
-        },
-        async signIn({user} : {user : AdapterUser | User}){
+    callbacks: {
+        async session({ session }) {
+            const email = session?.user?.email as string;
             try{
+                const data = await getUser(email) as {user?: UserProfile }
+
+                const newSession = {
+                    ...session,
+                    user:{
+                        ...session.user,
+                        ...data?.user
+                    }
+                }
+                return newSession;
+            }catch(err){
+                console.log(err)
+                return session;
+            }
+        },
+        async signIn({ user }: { user: AdapterUser | User }) {
+            try {
+
+                const userExists = await getUser(user?.email as string) as { user?: UserProfile }
+
+                if (!userExists.user) {
+                    await createUser(user.name as string, user.email as string, user.image as string)
+                }
 
                 return true
-            }catch(error : any){
+            } catch (error: any) {
                 console.log(error)
 
                 return false
             }
-        }       
+        }
     }
 }
 
 
 export async function getCurrentUser() {
     const session = await getServerSession(authOption) as SessionInterface
-    
+
     return session
 }
