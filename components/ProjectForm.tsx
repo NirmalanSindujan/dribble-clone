2533e5b0bdea5b4866a64a8cdabed62ac64f57e3
Sindujan <sindujan@paraqum.com>
2023-09-13 15:35:29 +0530

    add projects

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,7 +1,100 @@
-import React from 'react'
+'use client'
+import { SessionInterface } from '@/common.types'
+import React, { ChangeEvent } from 'react'
+import { FormField } from './FormField'
 
-export const ProjectForm = () => {
+type Props = {
+    type: string,
+    session: SessionInterface
+}
+
+export const ProjectForm = ({ type, session }: Props) => {
+
+    const image = null
+    const handleSubmit = (e: React.FormEvent) => {
+
+    }
+
+    const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+
+    }
+
+    const handleStateChange = (fieldName: string, value: string) => {
+
+    }
+
+    const form = {
+        image: '',
+        title: ''
+    }
+
+
     return (
-        <div>ProjectForm</div>
+        <form
+            onSubmit={handleSubmit}
+            className='flexStart form'
+        >
+            <div className='flexStart form_image-container'>
+                <label html_Form="poster" className='flexCenter form_image-label'>
+                    {!image && "Choose Your image"}
+                </label>
+                <input
+                    id="image"
+                    type='file'
+                    accept='image/*'
+                    required={type === "create"}
+                    className='form_image-input'
+                    onChange={handleChangeImage}
+                />
+            </div>
+
+
+            <FormField
+                title="Title"
+                state={form.title}
+                placeholder="Flexxible"
+                setState={(value) => handleStateChange('title', value)}
+            />
+
+            <FormField
+                title="Description"
+                state={form.description}
+                placeholder="Your Description Here..."
+                setState={(value) => handleStateChange('description', value)}
+            />
+
+            <FormField
+                type={"url"}
+                title="Website URL"
+                state={form.liveSiteUrl}
+                placeholder="Your Url Dude"
+                setState={(value) => handleStateChange('liveSiteUrl', value)}
+            />
+
+            <FormField
+                type={"url"}
+                title="GitHub URL"
+                state={form.githubUrl}
+                placeholder="Github please..."
+                setState={(value) => handleStateChange('githubUrl', value)}
+            />
+
+            <FormField
+                type={"url"}
+                title="GitHub URL"
+                state={form.githubUrl}
+                placeholder="Github please..."
+                setState={(value) => handleStateChange('githubUrl', value)}
+            />
+
+
+
+
+<div className='flexStart w-full'>
+    <button> Create</button>
+
+</div>
+
+        </form>
     )
 }
