1fc066ecca56a7629631ea9a32c74b1088fc019c
Sindujan <sindujan@paraqum.com>
2023-09-13 11:58:54 +0530

    session and authentications

--- lib/session.ts
+++ lib/session.ts
@@ -15,14 +15,21 @@ export const authOption: NextAuthOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
         })
     ],
-    // jwt :{
-    //     encode:({secret,token})=>{
+    jwt :{
+        encode:({secret,token})=>{
+            const encodedToken = jsonwebtoken.sign({
+                ...token,
+                iss : 'grafbase',
+                exp : Math.floor(Date.now()/1000)+60*60
+            },secret)
+            return encodedToken;
+        },
+        decode: async({secret,token})=>{
+            const decodedToken = jsonwebtoken.verify(token!,secret)as JWT;
 
-    //     },
-    //     decode: async({secret,token})=>{
-
-    //     }
-    // },
+            return decodedToken;
+        }
+    },
     theme: {
         colorScheme: "light",
         logo: '/logo.png'
